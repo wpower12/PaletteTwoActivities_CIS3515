541f82088a98620156ca1a8cb78ab3ecd3c69fd6
William Power <willpowe@gmail.com>
2017-02-15 13:41:06 -0500

    Initial commit

---
+++ app/src/main/java/com/poweriii/palettetwoactivities/PaletteActivity.java
@@ -0,0 +1,13 @@
+package com.poweriii.palettetwoactivities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PaletteActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palette);
+    }
+}
